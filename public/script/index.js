6f74d2d4aff43ac1d4b4798f635f7105a378fc97
Johan Djärv Karltorp <johan.djarv.karltorp@gmail.com>
2020-06-11 15:54:15 +0200

    added navigation between images with arrow keys

--- public/script/index.js
+++ public/script/index.js
@@ -1,4 +1,43 @@
+/* global M */
+
+
 document.addEventListener('DOMContentLoaded', () => {
     let elements = document.querySelectorAll('.materialboxed');
-    let instances = M.Materialbox.init(elements);
+    let listener;
+    let options = {
+        onOpenEnd: (target) => {
+            let index = Array.prototype.indexOf.call(elements, target);
+
+            listener = navigation.bind(null, target, index, elements);
+
+            document.addEventListener('keyup', listener, {once: true});
+        },
+        onCloseStart: () => {
+            document.removeEventListener('keyup', listener);
+        }
+    };
+
+    M.Materialbox.init(elements, options);
 });
+
+
+let navigation = (target, index, elements, event) => {
+    switch (event.key) {
+        case "ArrowLeft":
+            M.Materialbox.getInstance(target).close();
+
+            if (index > 0) {
+                M.Materialbox.getInstance(elements.item(index - 1)).open();
+            }
+
+            break;
+
+        case "ArrowRight":
+            M.Materialbox.getInstance(target).close();
+
+            if (index < elements.length - 1) {
+                M.Materialbox.getInstance(elements.item(index + 1)).open();
+            }
+            break;
+    }
+};
